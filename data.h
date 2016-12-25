c0b51b0e5d0bebf0fb74ba47bb723ff3eb548e57
SlavaLapin <vechyaslaugh@gmail.com>
2016-12-26 00:10:04 +0300

    Update data.h

--- data.h
+++ data.h
@@ -7,15 +7,30 @@ typedef struct entry
 	unsigned int number;
 	unsigned int exp_m;
 	unsigned int exp_y;
-	entry* prev;
-	void (delete*) ();
+	entry* prev; //stores a pointer to the previous entry with the same name
 	
 	
 }
 
-void search (char mode);
+typedef struct book
+{
+	entry* top;
+	unsigned int length;
+	void (delete*) (*book);
+	void (add*) (*book);
+	void (find*) (char, *book);
+	void (load*) (*book);
+	void (save*) (*book);
+}	
 
-void create ();
+void search (char mode, *book b);
 
+void create (*book b);
 
+void destroy (*book b);
+
+void loadf (*book b, char* filename);
+
+void savef (book* b);
 	
+#endif DATA_H
